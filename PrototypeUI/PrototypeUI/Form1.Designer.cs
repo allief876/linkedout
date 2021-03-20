841ca94285e7a1257e4e6e5a8181fdcf78636416
RayhanAsadel <rayhanasadel@gmail.com>
2021-03-20 21:15:06 +0700

    add prototypeUI
    baru sampe browse file

---
+++ PrototypeUI/PrototypeUI/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿
+namespace PrototypeUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Lbl_Judul = new System.Windows.Forms.Label();
+            this.Lbl_GraphFile = new System.Windows.Forms.Label();
+            this.lbl__Algorithm = new System.Windows.Forms.Label();
+            this.btn_browse = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.Lbl_ChooseAcc = new System.Windows.Forms.Label();
+            this.Lbl_ExploreFriends = new System.Windows.Forms.Label();
+            this.Cbb_2 = new System.Windows.Forms.ComboBox();
+            this.Cbb_1 = new System.Windows.Forms.ComboBox();
+            this.btn_submit = new System.Windows.Forms.Button();
+            this.Lbl_FriendsRecom = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Lbl_Judul
+            // 
+            this.Lbl_Judul.Font = new System.Drawing.Font("Arial Black", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl_Judul.Location = new System.Drawing.Point(215, 9);
+            this.Lbl_Judul.Name = "Lbl_Judul";
+            this.Lbl_Judul.Size = new System.Drawing.Size(318, 39);
+            this.Lbl_Judul.TabIndex = 0;
+            this.Lbl_Judul.Text = "People You May Know";
+            // 
+            // Lbl_GraphFile
+            // 
+            this.Lbl_GraphFile.AutoSize = true;
+            this.Lbl_GraphFile.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl_GraphFile.Location = new System.Drawing.Point(79, 88);
+            this.Lbl_GraphFile.Name = "Lbl_GraphFile";
+            this.Lbl_GraphFile.Size = new System.Drawing.Size(103, 22);
+            this.Lbl_GraphFile.TabIndex = 1;
+            this.Lbl_GraphFile.Text = "Graph File:";
+            this.Lbl_GraphFile.Click += new System.EventHandler(this.label2_Click_1);
+            // 
+            // lbl__Algorithm
+            // 
+            this.lbl__Algorithm.AutoSize = true;
+            this.lbl__Algorithm.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl__Algorithm.Location = new System.Drawing.Point(79, 133);
+            this.lbl__Algorithm.Name = "lbl__Algorithm";
+            this.lbl__Algorithm.Size = new System.Drawing.Size(95, 22);
+            this.lbl__Algorithm.TabIndex = 2;
+            this.lbl__Algorithm.Text = "Algorithm:";
+            // 
+            // btn_browse
+            // 
+            this.btn_browse.Location = new System.Drawing.Point(232, 84);
+            this.btn_browse.Name = "btn_browse";
+            this.btn_browse.Size = new System.Drawing.Size(93, 34);
+            this.btn_browse.TabIndex = 3;
+            this.btn_browse.Text = "Browse";
+            this.btn_browse.UseVisualStyleBackColor = true;
+            this.btn_browse.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(18, 12);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(46, 17);
+            this.radioButton1.TabIndex = 4;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "DFS";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(70, 12);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(45, 17);
+            this.radioButton2.TabIndex = 5;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "BFS";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Location = new System.Drawing.Point(232, 126);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(141, 30);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            // 
+            // Lbl_ChooseAcc
+            // 
+            this.Lbl_ChooseAcc.AutoSize = true;
+            this.Lbl_ChooseAcc.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl_ChooseAcc.Location = new System.Drawing.Point(87, 436);
+            this.Lbl_ChooseAcc.Name = "Lbl_ChooseAcc";
+            this.Lbl_ChooseAcc.Size = new System.Drawing.Size(155, 22);
+            this.Lbl_ChooseAcc.TabIndex = 7;
+            this.Lbl_ChooseAcc.Text = "Choose Account:";
+            this.Lbl_ChooseAcc.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // Lbl_ExploreFriends
+            // 
+            this.Lbl_ExploreFriends.AutoSize = true;
+            this.Lbl_ExploreFriends.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl_ExploreFriends.Location = new System.Drawing.Point(87, 473);
+            this.Lbl_ExploreFriends.Name = "Lbl_ExploreFriends";
+            this.Lbl_ExploreFriends.Size = new System.Drawing.Size(192, 22);
+            this.Lbl_ExploreFriends.TabIndex = 8;
+            this.Lbl_ExploreFriends.Text = "Explore Friends With:";
+            this.Lbl_ExploreFriends.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // Cbb_2
+            // 
+            this.Cbb_2.FormattingEnabled = true;
+            this.Cbb_2.Location = new System.Drawing.Point(328, 474);
+            this.Cbb_2.Name = "Cbb_2";
+            this.Cbb_2.Size = new System.Drawing.Size(121, 21);
+            this.Cbb_2.TabIndex = 10;
+            this.Cbb_2.Text = "Nama SImpul";
+            // 
+            // Cbb_1
+            // 
+            this.Cbb_1.FormattingEnabled = true;
+            this.Cbb_1.Location = new System.Drawing.Point(328, 436);
+            this.Cbb_1.Name = "Cbb_1";
+            this.Cbb_1.Size = new System.Drawing.Size(121, 21);
+            this.Cbb_1.TabIndex = 11;
+            this.Cbb_1.Text = "NamaSimpul";
+            // 
+            // btn_submit
+            // 
+            this.btn_submit.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_submit.Location = new System.Drawing.Point(91, 507);
+            this.btn_submit.Name = "btn_submit";
+            this.btn_submit.Size = new System.Drawing.Size(123, 34);
+            this.btn_submit.TabIndex = 12;
+            this.btn_submit.Text = "Submit";
+            this.btn_submit.UseVisualStyleBackColor = true;
+            // 
+            // Lbl_FriendsRecom
+            // 
+            this.Lbl_FriendsRecom.AutoSize = true;
+            this.Lbl_FriendsRecom.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl_FriendsRecom.Location = new System.Drawing.Point(87, 560);
+            this.Lbl_FriendsRecom.Name = "Lbl_FriendsRecom";
+            this.Lbl_FriendsRecom.Size = new System.Drawing.Size(234, 22);
+            this.Lbl_FriendsRecom.TabIndex = 13;
+            this.Lbl_FriendsRecom.Text = "Friends Recommendation:";
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Arial Black", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(88, 180);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(596, 227);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "PlaceHolder Visualisasi Graph";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(742, 750);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Lbl_FriendsRecom);
+            this.Controls.Add(this.btn_submit);
+            this.Controls.Add(this.Cbb_1);
+            this.Controls.Add(this.Cbb_2);
+            this.Controls.Add(this.Lbl_ExploreFriends);
+            this.Controls.Add(this.Lbl_ChooseAcc);
+            this.Controls.Add(this.btn_browse);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lbl__Algorithm);
+            this.Controls.Add(this.Lbl_GraphFile);
+            this.Controls.Add(this.Lbl_Judul);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Lbl_Judul;
+        private System.Windows.Forms.Label Lbl_GraphFile;
+        private System.Windows.Forms.Label lbl__Algorithm;
+        private System.Windows.Forms.Button btn_browse;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label Lbl_ChooseAcc;
+        private System.Windows.Forms.Label Lbl_ExploreFriends;
+        private System.Windows.Forms.ComboBox Cbb_2;
+        private System.Windows.Forms.ComboBox Cbb_1;
+        private System.Windows.Forms.Button btn_submit;
+        private System.Windows.Forms.Label Lbl_FriendsRecom;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ PrototypeUI/PrototypeUI/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace PrototypeUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e) //Browse
+        {
+            
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            int size = -1;
+            DialogResult result = openFileDialog1.ShowDialog(); // Show the dialog.
+            if (result == DialogResult.OK) // Test result.
+            {
+                string file = openFileDialog1.FileName;
+                try
+                {
+                    string text = File.ReadAllText(file);
+                    size = text.Length;
+                    Console.WriteLine(text);
+                }
+                catch (IOException)
+                {
+                }
+            }
+            Console.WriteLine(size); // <-- Shows file size in debugging mode.
+            Console.WriteLine(result); // <-- For debugging use.
+           
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+    }
+}
